f7e55e39b726c6674d33fd9d919dad3f07561aaa
Justin Doan <jmdoan1@gmail.com>
2018-12-01 19:42:15 -0500

    #2: Initial ProjectThumbnail component creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ProjectThumbnail from './components/ProjectThumbnail/ProjectThumbnail';
 import './App.css';
 
 import logo from './logo.svg';
@@ -9,11 +10,18 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Justin Doan</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <ProjectThumbnail
+          name="WatchFlippers"
+          tagline="An app"
+          personal={false}
+          client={true}
+          W2={false} 
+        />
       </div>
     );
   }
---
+++ src/components/ProjectThumbnail/ProjectThumbnail.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+export interface Props {
+    name: string;
+    images?: HTMLImageElement[];
+    gifs?: HTMLImageElement[];
+    tagline?: string;
+    description?: string;
+    personal: boolean;
+    client: boolean;
+    W2: boolean;
+    dateStart?: Date;
+    dateMVP?: Date;
+}
+
+export default class ProjectThumbnail extends React.PureComponent<Props> {
+    render() {
+        return (
+            <div className="ProjectThumbnail">
+                <h1>{this.props.name}</h1>
+                <h4>{this.props.tagline}</h4>
+                <text>{this.props.personal ? "Personal" : this.props.client ? "Client" : this.props.W2 ? "W2" : ""}</text>
+            </div>
+        );
+    }
+}
