45cfabfa402b4ef75af3c9cd332660622fc79313
Justin Doan <jmdoan1@gmail.com>
2018-12-04 21:09:49 -0500

    #5: Initial mockup

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,13 @@ import * as React from 'react';
 import ProjectThumbnail from './components/ProjectThumbnail/ProjectThumbnail';
 import './App.css';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Justin Doan</h1>
+          <h1 className="HeaderName">Justin Doan</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <div>
           <ProjectThumbnail
             name="WatchFlippers"
