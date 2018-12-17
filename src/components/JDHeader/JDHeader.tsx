7e9e4bbdc7dca8cd73438b44a9718f2b75a3be49
Justin Doan <jmdoan1@gmail.com>
2018-12-16 20:34:33 -0500

    #6: Header Fixed(ish)

--- src/components/JDHeader/JDHeader.tsx
+++ src/components/JDHeader/JDHeader.tsx
@@ -9,7 +9,7 @@ export default class ProjectThumbnail extends React.PureComponent<Props> {
     render() {
         return(
             <header className="JDHeader">
-                <table className="JDHeaderTable">
+                <table cellSpacing={0} cellPadding={0} className="JDHeaderTable">
                     <tr>
                         <td>
                             <p className="JDHeaderName">{this.props.name}</p>
@@ -17,8 +17,11 @@ export default class ProjectThumbnail extends React.PureComponent<Props> {
                         <td className="JDHeaderButton">
                             <p>test 1</p>
                         </td>
-                        <td>
-                            <button>test 2</button>
+                        <td className="JDHeaderButton">
+                            <p>test 2</p>
+                        </td>
+                        <td className="JDHeaderButton">
+                            <p>test 3</p>
                         </td>
                     </tr>
                 </table>
